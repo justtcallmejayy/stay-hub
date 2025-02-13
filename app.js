99523aaa4f372c23707521f450186375e5faebc5
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-02-12 23:57:37 -0500

    Connected to MONGODB

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const PORT = 8080;
+const DB_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main().then(()=>{
+    console.log("Connected to database");
+}).catch(()=>{
+    console.log("Error connecting to database");
+});
+
+async function main() {
+    await mongoose.connect(DB_URL);
+    
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World, from root");
